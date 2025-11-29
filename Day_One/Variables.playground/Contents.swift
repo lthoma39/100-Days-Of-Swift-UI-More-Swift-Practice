f7b6047ba32e07aed3f3a94b44fc7886dce72458
lthoma39 <lthoma39@uic.edu>
2025-11-29 14:40:46 -0600

    fixing branch issue

---
+++ Day_One/Variables.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var myName = "Latrell"
+
+var lesson = "Var is how we declare variables. Don't need to type the keyword itself when mentioning the variable unless we are declaring a new var"
+
+let birthMonth = "September"
+
+print(birthMonth)
+
+let camelCaseConvention = "practicing!"
+
+print(camelCaseConvention)

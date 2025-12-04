2d9e8d2aca0a8744940cb570eefd331f422ac937
lthoma39 <lthoma39@uic.edu>
2025-12-04 08:58:21 -0600

    Day 4 Content

---
+++ Day 4/MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+let firstName = "Latrell"
+let phoneNum = 18005524900
+let lastName: String = "Thomas"
+let time: Int = 7
+
+var favSongsArr: [String] = []
+favSongsArr.append("Swish")
+print(favSongsArr)
+
+var artistIsRetired: [String: Bool] = [:]
+artistIsRetired["Jay Z"] = true
+print(artistIsRetired)
+
+var books: Set<String> = []
+books.insert("IT")
+print(books.contains("IT"))
+
+let a: String
+var b: String

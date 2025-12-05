89e1ae061aeda08bc8cb7da128bba6a0a009d562
lthoma39 <lthoma39@uic.edu>
2025-12-05 01:48:51 -0600

    more swift practice. taking a break to learn TS for business

---
+++ Day 6/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+var weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+for weekday in weekdays {
+    print(weekday)
+}
+
+for i in 0..<weekdays.count {
+    print(weekdays[i])
+}
+
+for name in ["Tyler", "Rob", "Tom"]{
+    print(name)
+}
+for i in stride(from: 0, through: 10, by: 2){
+    print(i)
+}
+var greeting = "Hello, playground"
+

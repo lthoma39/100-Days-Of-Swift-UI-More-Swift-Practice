a138daaf3885ab836607405f83005a347260e63a
lthoma39 <lthoma39@uic.edu>
2025-12-05 23:05:04 -0600

    Fixing file consistency

---
+++ Day 5/MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+if (true) {
+    print("What are you testing")
+}
+
+let isVisible: Bool = false
+
+let buttonToggle = isVisible ? "On" : "Off"
+
+print(buttonToggle)
+
+var greeting = "Hello, playground"
